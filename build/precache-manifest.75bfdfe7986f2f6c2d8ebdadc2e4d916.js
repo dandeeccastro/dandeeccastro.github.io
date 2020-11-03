f89c26cd86cc0b99620572528410b7277e2f84fd
Danilo <dundeepessoal@gmail.com>
2020-11-03 01:17:53 -0300

    Refazendo o build

---
+++ build/precache-manifest.75bfdfe7986f2f6c2d8ebdadc2e4d916.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58cfbc5e5152a827a67726aa2c635781",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e6b9d5ea890b73079b2",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "62742b3e3ef296f20ab3",
+    "url": "/static/css/main.52e66a75.chunk.css"
+  },
+  {
+    "revision": "3e6b9d5ea890b73079b2",
+    "url": "/static/js/2.4d4bb8ac.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.4d4bb8ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62742b3e3ef296f20ab3",
+    "url": "/static/js/main.ead90faf.chunk.js"
+  },
+  {
+    "revision": "bc7e7b9429bf9c8b8f76",
+    "url": "/static/js/runtime-main.90aafbb6.js"
+  }
+]);
